d5bea5aa506494953baa3da6b466491a22661006
marcoantoniorosario <marcoantoniorosario.academico@gmail.com>
2023-07-31 15:09:30 -0300

    projeto

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Header from './src/components/Header';
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header />
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -18,3 +17,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
